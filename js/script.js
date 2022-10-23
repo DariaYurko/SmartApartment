24ee102bf4cba2150383c7fa245e3193c4a6a471
unknown <dariayurkodev@gmail.com>
2022-10-23 13:02:46 +0300

    Finished section 'Property' with adaptive and script-code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// =============================== Slider =================================
 const swiper = new Swiper('.swiper', {
    // Optional parameters
    loop: true,
@@ -7,5 +8,37 @@ const swiper = new Swiper('.swiper', {
      nextEl: '.swiper-button-next',
      prevEl: '.swiper-button-prev',
    },
- 
  });
+// ==========================================================================
+
+// ============================= Tabs =======================================
+
+const tabsBtn = document.querySelectorAll(".tabs__btn");
+const tabsItems = document.querySelectorAll(".tabs__item")
+
+tabsBtn.forEach(function(tabBtn) {
+
+  tabBtn.addEventListener("click", function() {
+    let currentBtn = tabBtn
+    let tabId = currentBtn.getAttribute("data-tab")
+    let currentItem = document.querySelector(tabId)
+
+    if (!currentBtn.classList.contains("active")) {
+      tabsBtn.forEach(function(tabBtn) {
+        if (tabBtn.classList.contains("active")) {
+          tabBtn.classList.remove("active")
+        } 
+      })
+  
+      tabsItems.forEach(function(tabItem) {
+        if (tabItem.classList.contains("active")) {
+          tabItem.classList.remove("active")
+        }
+      })
+  
+      currentBtn.classList.add("active")
+      currentItem.classList.add("active")
+    }
+
+  })
+})
